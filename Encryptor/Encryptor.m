912d2c4ff4310d6e7a9f74e1634dbd7577939fe1
Alexander Slavschik <alexander.slavschik@monterosa.co.uk>
2013-02-27 17:12:08 +0300

    generateKeyPair removed

--- Encryptor/Encryptor.m
+++ Encryptor/Encryptor.m
@@ -102,51 +102,6 @@ const uint32_t keySize = 512;
     return [[NSString alloc] initWithData:decryptedData encoding:NSUTF8StringEncoding];
 }
 
-- (void) generateKeyPair:(NSUInteger)keySize {
-    OSStatus sanityCheck = noErr;
-    publicKey = NULL;
-    privateKey = NULL;
-    
-    // Container dictionaries.
-    NSMutableDictionary * privateKeyAttr = [[NSMutableDictionary alloc] init];
-    NSMutableDictionary * publicKeyAttr = [[NSMutableDictionary alloc] init];
-    NSMutableDictionary * keyPairAttr = [[NSMutableDictionary alloc] init];
-    
-    // Set top level dictionary for the keypair.
-    [keyPairAttr setObject:(__bridge id)kSecAttrKeyTypeRSA forKey:(__bridge id)kSecAttrKeyType];
-    [keyPairAttr setObject:[NSNumber numberWithUnsignedInteger:keySize] forKey:(__bridge id)kSecAttrKeySizeInBits];
-    
-    // Set the private key dictionary.
-    [privateKeyAttr setObject:[NSNumber numberWithBool:YES] forKey:(__bridge id)kSecAttrIsPermanent];
-    [privateKeyAttr setObject:privateTag forKey:(__bridge id)kSecAttrApplicationTag];
-    
-    // Set the public key dictionary.
-    [publicKeyAttr setObject:[NSNumber numberWithBool:YES] forKey:(__bridge id)kSecAttrIsPermanent];
-    [publicKeyAttr setObject:publicTag forKey:(__bridge id)kSecAttrApplicationTag];
-    
-    // Set attributes to top level dictionary.
-    [keyPairAttr setObject:privateKeyAttr forKey:(__bridge id)kSecPrivateKeyAttrs];
-    [keyPairAttr setObject:publicKeyAttr forKey:(__bridge id)kSecPublicKeyAttrs];
-    
-    // SecKeyGeneratePair returns the SecKeyRefs just for educational purposes.
-    sanityCheck = SecKeyGeneratePair((__bridge CFDictionaryRef)keyPairAttr, &publicKey, &privateKey);
-    NSError *error;
-    if(sanityCheck != noErr){
-        error = [[NSError alloc] initWithDomain:NSOSStatusErrorDomain code:sanityCheck userInfo:nil];
-    }else{
-        if(publicKey == NULL || privateKey == NULL)
-        {
-            NSDictionary *userInfo = [NSDictionary dictionaryWithObjects:@[[NSNumber numberWithBool:publicKey != NULL], [NSNumber numberWithBool:privateKey != NULL]]
-                                                                 forKeys:@[@"has_public", @"has_private"]];
-            error = [[NSError alloc] initWithDomain:@"Encryptor" code:1 userInfo:userInfo];
-        }
-    }
-    if(error){
-        //handle error here, if you want
-        @throw [[NSException alloc] initWithName:[error localizedDescription] reason:[error localizedFailureReason] userInfo:nil];
-    }
-}
-
 #pragma mark -
 
 - (SecKeyRef) getPublicKeyRef {
