9035075da85244cc527b296b850ffdc3e792ca17
Alexander Slavschik <alexander.slavschik@monterosa.co.uk>
2013-03-04 19:05:23 +0300

    Cleanup project.

---
+++ iOS-encryption/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS-encryption
+//
+//  Created by Alexander Slavschik on 04.03.13.
+//  Copyright (c) 2013 Alexander Slavschik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    //return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
